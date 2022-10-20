2415cd93c8ba50903dae008cdb2bea37c6634401
Punk73 <teguhsetiawan171@gmail.com>
2022-10-20 13:30:42 +0700

    berhasil merapikan file berdasarkan hari

---
+++ index.py
@@ -0,0 +1,56 @@
+
+# get sub dir 
+
+# loop through sub dir
+
+# if contain date, make new folder based on that specific date
+
+# move to that folder
+from datetime import datetime
+import os
+import re 
+from distutils.dir_util import copy_tree
+
+def get_immediate_subdirectories(a_dir):
+    
+    res = [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
+
+    return sorted(res)
+
+def getdate(folder, reg = '\d+-\d+-\d+'):
+    # is contain date
+
+    # check if that day folder exists if not create
+
+    # move this folder to that folder / or atleast copy
+    res = re.search(reg, folder)
+    if res:
+        res = res.group(0)
+        return res.strip()
+    else:
+        return ''
+
+def move():
+    monthFolder= input("input directory untuk dirapikan : ") # "./agustus"
+    subdir = get_immediate_subdirectories(monthFolder)
+    # print(subdir)
+    res = []
+    for folder in subdir:
+        # print(folder)
+        dt = getdate(folder)
+        if dt != '':
+            dtobj =  datetime.strptime(dt, '%Y-%m-%d')
+        else:
+            continue
+
+        new_format = dtobj.strftime('%d %B')
+        new_folder = monthFolder +'/results/'+ new_format
+        if not os.path.exists(new_folder):
+            os.makedirs(new_folder)
+
+        # print(new_format, folder)
+        copy_tree(  monthFolder + "/"+ folder, new_folder + '/' + folder )
+        # new_folder = 
+
+move()
---
+++ readme.md
@@ -0,0 +1,7 @@
+
+
+## how to use ##
+1. run source venv/bin/activate to activate virtual environtment
+2. run python3 index.py
+3. copas direcotory yang ingin dirapikan.
+4. hasil ada di folder results dalam folder yang dirapikan tersebut.
